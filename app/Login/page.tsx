c90bdf8de56892b348eb83bc5399eff7ccd66df4
unknown <aric060205@gmail.com>
2025-06-12 07:25:47 +0700

    feat: add go without login button

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
             className="max-w-full h-auto mb-8" // Adjusted margin
           />
           <div className="w-full flex justify-center"> {/* Added a wrapper for the button */}
-            <Link href="/home"
+            <Link href="/home_not_logging_in"
               className="bg-[#a56f2a] text-white font-semibold text-sm px-5 py-2 rounded flex items-center gap-2 hover:bg-[#8a5a20] transition">
                 Lanjutkan Tanpa Melakukan Login
                 <i className="fas fa-arrow-right"></i>
@@ -54,12 +54,14 @@ export default function LoginPage() {
               className="w-full border border-gray-300 rounded-md px-4 py-2 mb-10 text-gray-400 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#5F8632]"
             />
 
-            <button
-              type="submit"
-              className="w-full bg-[#5F8632] text-white font-bold text-lg rounded-md py-3 mb-4 hover:bg-[#4a6a26] transition-colors"
-            >
-              Login
-            </button>
+            <Link href="/home">
+              <button
+                type="button"
+                className="w-full bg-[#5F8632] text-white font-bold text-lg rounded-md py-3 mb-4 hover:bg-[#4a6a26] transition-colors"
+              >
+                Login
+              </button>
+            </Link>
 
             <p className="text-center text-black text-sm">
               Belum punya akun?{' '}
